f63407eedb215c46aa369c670d2c8ea2545a23f8
Φαρμακη Ευτυχια <44114768+eftichiafarmaki@users.noreply.github.com>
2018-12-17 00:00:13 +0200

    Update script_1.js

--- assets/scripts/script_1.js
+++ assets/scripts/script_1.js
@@ -44,8 +44,8 @@ function donutChart() {
             // contructs and arc generator. This will be used for the donut. The difference between outer and inner
             // radius will dictate the thickness of the donut
             var arc = d3.arc()
-                .outerRadius(radius * 0.8)
-                .innerRadius(radius * 0.6)
+                .outerRadius(radius * 6)
+                .innerRadius(radius * 3)
                 .cornerRadius(cornerRadius)
                 .padAngle(padAngle);
 
