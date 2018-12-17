fceb8542b8741e11c3eafec0ae74e465eefebe0c
Φαρμακη Ευτυχια <44114768+eftichiafarmaki@users.noreply.github.com>
2018-12-17 03:00:55 +0200

    Update script_1.js

--- assets/scripts/script_1.js
+++ assets/scripts/script_1.js
@@ -45,7 +45,7 @@ function donutChart() {
             // radius will dictate the thickness of the donut
             var arc = d3.arc()
                 .outerRadius(radius * 1.0)
-                .innerRadius(radius * 0.3)
+                .innerRadius(radius * 0.2)
                 .cornerRadius(cornerRadius)
                 .padAngle(padAngle);
 
